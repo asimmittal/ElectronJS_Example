9b567717d8ccfd008e27e6cd182d6dd904b2cf4f
Asim Mittal <asim.mittal@gmail.com>
2017-04-30 19:44:18 -0400

    documentation + some cleanup to the UI

--- app/renderer.js
+++ app/renderer.js
@@ -5,41 +5,81 @@ const ipc = electron.ipcRenderer;
 var results = {};
 var total = 0;
 
-//get the button
+//get the button + prog loader
 var btnSelectFolder = document.getElementById("btnPickFolder");
+var progLoader = document.getElementById("loader");
 
-//attach a click handler to it
-//when the user clicks this button, we'll notify the main process
+/**********************************************************************************
+ * event handler: click on "Select Directory" button
+ * when clicked, request the main process for a file open dialog
+ **********************************************************************************/
 btnSelectFolder.addEventListener('click', function(e){
-    results = {};
-    total = 0;
+    init();
     ipc.send("ipcEvent_requestFileDialog");
 });
 
-//get the loader icon
-var progLoader = document.getElementById("loader");
+/**********************************************************************************
+ * handleScanStart()
+ * handle UI changes for when the main process begins scanning the root dir
+ **********************************************************************************/
+function handleScanStart(){
+    progLoader.style.display = "block";
+    btnSelectFolder.disabled = true;
+}
 
-//prog loader visibility functions
-progLoader.show = function(){ progLoader.style.display = "block"; }
-progLoader.hide = function(){ progLoader.style.display = "none"; }
+/**********************************************************************************
+ * handleScanEnd()
+ * handle UI changes for when the main process ends the scan
+ **********************************************************************************/
+function handleScanEnd(){
+    progLoader.style.display = "none";
+    btnSelectFolder.disabled = false;
+}
 
-//row template for results table
-var rowTemplate = `<tr>
-                    <td width="10%">{ext}</td>
-                    <td width="30%">{count}</td>
-                    <td width="60%"><div class="bar" style='width: {pct}%'></div></td>
-                  </tr>`;
+/**********************************************************************************
+ * getTitleRowTemplate(ex,cnt,pct)
+ * return the row template for the first row
+ **********************************************************************************/
+function getTitleRowTemplate(){
+    //row template for results table
+    var rowTemplate = `<tr>
+                        <td width="10%"><b>Extensions</b></td>
+                        <td width="30%"><b>File Count</b></td>
+                        <td width="60%"><b>Percentage</b></td>
+                    </tr>`;
 
+    return rowTemplate;
+}
+
+
+/**********************************************************************************
+ * getRowTemplate(ex,cnt,pct)
+ * return the row template for a specified extension, filecount and 
+ * percentage value
+ **********************************************************************************/
 function getRowTemplateFor(ext,count,pct){
+    //row template for results table
+    var rowTemplate = `<tr>
+                        <td width="10%">{ext}</td>
+                        <td width="30%">{count}</td>
+                        <td width="60%"><div class="bar" style='width: {pct}%'></div></td>
+                    </tr>`;
+
     return rowTemplate.replace("{ext}",ext).replace("{count}", count).replace("{pct}",pct);
 }
 
+/**********************************************************************************
+ * draw()
+ * redraw the table of results in the UI
+ **********************************************************************************/
 function draw(){
     var table = document.getElementById("results");
     if(table){
 
-        table.innerHTML = '';
+        //add the title row
+        table.innerHTML = getTitleRowTemplate();
 
+        //for each extension in results, add a row
         for(var ext in results){
             var count = results[ext];
             var pct = count / total * 100;
@@ -47,6 +87,7 @@ function draw(){
             table.innerHTML += row;
         }
 
+        //update total
         var lblTotal = document.getElementById("lblTotal");
         if(total && lblTotal){
             lblTotal.innerHTML = "Total: " +total+ " files found";
@@ -54,6 +95,10 @@ function draw(){
     }
 }
 
+/**********************************************************************************
+ * update(e,stats)
+ * update the results object with current data
+ **********************************************************************************/
 function update(e,stats){
     for(var i in stats){
         var name = stats[i].name;
@@ -71,7 +116,19 @@ function update(e,stats){
     draw();
 }
 
+/**********************************************************************************
+ * init()
+ * reset the state of the UI
+ **********************************************************************************/
+function init(){
+    results = {};
+    total = 0;
+    update();
+}
 
-ipc.on("ipcEvent_startScan", progLoader.show);
-ipc.on("ipcEvent_endScan", progLoader.hide);
+/**********************************************************************************
+ * IPC events sent by the main process
+ **********************************************************************************/
+ipc.on("ipcEvent_startScan", handleScanStart);
+ipc.on("ipcEvent_endScan", handleScanEnd);
 ipc.on("ipcEvent_files", update);
